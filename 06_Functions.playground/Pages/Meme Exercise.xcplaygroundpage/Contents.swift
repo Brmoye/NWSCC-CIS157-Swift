c1e392176312c0056912a98a93f4912322fecaa6
brmoye <brmoye09@gmail.com>
2022-12-30 17:32:56 -0600

    Initial Commit

---
+++ 06_Functions.playground/Pages/Meme Exercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+/*:
+ ## Functional Meme
+ 
+ Think of a fun song or meme that you've heard or seen recently. Try to think of one with repetitive parts. For example:
+ 
+ - A song with a repeated chorus.
+ - A meme that repeats a word or phrase.
+ - A song that repeats a line over and over.
+ 
+ Write out the song or meme using `print` statements, then look for patterns and repetition and choose groups of lines to combine into functions. This is a free-form exercise, so you can do whatever you like.
+*/
+func verseOne()
+{
+print("You are the love of my life")
+print("You take away my strife")
+}
+func verseTwo()
+{
+print("Don't ever leave me")
+print("Don't forget to feed me")
+}
+func chorus()
+{
+print("You take my breath away")
+print("You take my breath away")
+print("You take my breath away")
+print("You take my breath away")
+}
+
+func singTheSong()
+{
+    verseOne()
+    chorus()
+    verseTwo()
+    chorus()
+}
+
+singTheSong()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//: Next, make the meme your own.
+//:
+//: [Previous](@previous)  |  page 11 of 12  |  [Next: Personal Meme](@next)
